b5b2cc033937f8fb69ce829f444723a3fff64f65
clairezed <clairezuliani@gmail.com>
2018-06-10 19:04:22 +0200

    Improve geocoding appscript code with error basic management

--- appscripts/Code.js
+++ appscripts/Code.js
@@ -1,35 +1,52 @@
-function myFunction() {
+function addSpreadsheetTrigger() {
   var sheet = SpreadsheetApp.getActive();
-  ScriptApp.newTrigger("onFormSubmit")
+  ScriptApp.newTrigger("onFormSubmitTrigger")
    .forSpreadsheet(sheet)
    .onFormSubmit()
    .create();
 }
 
-function onFormSubmit(obj) { 
-  var address = formatAddress(obj);
+function onFormSubmitTrigger(event) { 
+  var address = formatAddress(event);
   var response = UrlFetchApp.fetch('https://api-adresse.data.gouv.fr/search/?q='+address);
-  var coordinates = getCoordinates(response);
-  var longitude = coordinates[0];
-  var latitude = coordinates[1];
-  var row = obj.range.getRow();
-  var latitudeRange = 'P'+row;
-  var longitudeRange = 'Q'+row;
-  s.getRange(latitudeRange).setValue(latitude);
-  s.getRange(longitudeRange).setValue(longitude);
+  var coordinates = getAddressCoordinates(response);
+  setRangeCoordinates(event, coordinates);
 }
 
-function formatAddress(obj) {
-  var street = obj.namedValues.Rue[0].replace(/ /g, '-' );
-  var zipcode = obj.namedValues['Code postal'][0];
-  var city = obj.namedValues['Ville'][0];
-  var address = [street, zipcode, city].join('-');
+
+function formatAddress(event) {
+  var street  = event.namedValues['Rue'][0];
+  var zipcode = event.namedValues['Code postal'][0];
+  var city    = event.namedValues['Ville'][0];
+  var address = [street, zipcode, city].join('-').replace(/\W/g, '-' );
   return address;
 }
 
-function getCoordinates(httpResponse) {
+function getAddressCoordinates(httpResponse) {
   var data = httpResponse.getContentText("UTF-8");
   var parsedData = JSON.parse(data)
-  var coordinates = parsedData.features[0].geometry.coordinates;
-  return coordinates;
+  if(parsedData.features[0]){
+    var coordinates = parsedData.features[0].geometry.coordinates;
+    return coordinates;
+  }else{
+    MailApp.sendEmail({
+      to: "clairezuliani@gmail.com",
+      subject: "[GuideConso] appscript error",
+      htmlBody: "No geocoding response for : "+ parsedData.query
+    });
+    return null; 
+  }
+}
+
+function setRangeCoordinates(event, coordinates) {
+  if(coordinates){
+    var longitude = coordinates[0];
+    var latitude = coordinates[1];
+    var row = event.range.getRow();
+    var latitudeRange = 'P' + row;
+    var longitudeRange = 'Q' + row;
+    var sheet = event.range.getSheet()
+    sheet.getRange(latitudeRange).setValue(latitude);
+    sheet.getRange(longitudeRange).setValue(longitude);
+  }
 }
